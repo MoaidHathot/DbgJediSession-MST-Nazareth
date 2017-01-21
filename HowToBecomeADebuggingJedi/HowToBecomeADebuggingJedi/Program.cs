aaa1d4d2698f0d98ff2b6f9860b0afdaf5d380d5
MoaidHathot <moaid.hathot@outlook.com>
2017-01-22 01:31:22 +0200

    code for ppt

--- HowToBecomeADebuggingJedi/HowToBecomeADebuggingJedi/Program.cs
+++ HowToBecomeADebuggingJedi/HowToBecomeADebuggingJedi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,48 @@ namespace HowToBecomeADebuggingJedi
     {
         static void Main(string[] args)
         {
+
+            Foo();
+            A.Foo();
+            B.Bar();
+            B.Foo(new B());
+            
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadKey();
         }
+
+        public static void Foo()
+        {
+            
+        }
+    }
+
+    class A
+    {
+        public static void Foo()
+        {
+            
+        }
+    }
+
+    class B
+    {
+        public static void Foo(B b)
+        {
+            b.Baz();
+        }
+
+        public void Baz()
+        {
+            
+        }
+
+        public static int Bar()
+        {
+            
+            
+            return 42;
+        }
+
     }
 }
